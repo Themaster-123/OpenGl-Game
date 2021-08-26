94a81b470d48791fabb8aad9f5b8f74683f27690
BuildTools <unconfigured@null.spigotmc.org>
2021-08-26 00:24:31 -0400

    Added more cubes

--- OpenGl Game/Main.cpp
+++ OpenGl Game/Main.cpp
@@ -12,8 +12,8 @@
 
 using namespace GLG;
 
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 800;
+unsigned int SCREEN_WIDTH = 800;
+unsigned int SCREEN_HEIGHT = 800;
 float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -81,7 +81,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif // __APPLE__
 	// window creation
-	GLFWwindow* window = createWindow(SCREEN_HEIGHT, SCREEN_HEIGHT, "Test");
+	GLFWwindow* window = createWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Test");
 
 	// load all OpenGl function pointers
 	loadOpenGlFunctions();
@@ -123,9 +123,7 @@ int main() {
 	shader.setInt("texture1", 0);
 	shader.setInt("texture2", 1);
 
-	glm::mat4 model = glm::mat4(1);
 	//model = glm::scale(model, glm::vec3(2, sin((float)glfwGetTime()), .5));
-	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(.5, .5, 0));
 
 	glm::mat4 view = glm::mat4(1);
 	view = glm::translate(view, glm::vec3(0, 0, -3.0f));
@@ -133,11 +131,25 @@ int main() {
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) SCREEN_WIDTH / (float) SCREEN_HEIGHT, 0.1f, 100.0f);
 
 	unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 	unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
 	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 	unsigned int projectionLoc = glGetUniformLocation(shader.ID, "projection");
 	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	glEnable(GL_DEPTH_TEST);
+
+	glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -145,17 +157,26 @@ int main() {
 
 		glClearColor(.3f, 0, .2f, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0, 0, 0, 0);
+		glClear(GL_DEPTH_BUFFER_BIT);
 		shader.use();
 		texture1.activate();
 		texture2.activate();
-		model = glm::mat4(1);
-		model = glm::rotate(model, (float)glfwGetTime() /4, glm::vec3(.5, .5, 0));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 		glBindVertexArray(VAO);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		glDrawArrays(GL_TRIANGLES, 0, 36/*6, GL_UNSIGNED_INT, 0*/);
+
+		for (int i = 0; i < sizeof(cubePositions) / sizeof(cubePositions[0]); i++) {
+			glm::mat4 model = glm::mat4(1);
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::rotate(model, (float)glfwGetTime() / 4 + glm::radians(i * 20.0f), glm::vec3(.5, .5, 0));
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+			glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+			glDrawArrays(GL_TRIANGLES, 0, 36/*6, GL_UNSIGNED_INT, 0*/);
+		}
+
+
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
@@ -166,6 +187,8 @@ int main() {
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	SCREEN_WIDTH = width;
+	SCREEN_HEIGHT = height;
 	glViewport(0, 0, width, height);
 }
 
