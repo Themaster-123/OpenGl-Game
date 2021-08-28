c7206443f510ca3b3708b67a13db7d9ebed32d16
BuildTools <unconfigured@null.spigotmc.org>
2021-08-28 17:34:18 -0400

    added a mesh class

---
+++ OpenGl Game/OpenGlGame/resources/mesh.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+#include "texture.h"
+#include "shader.h"
+
+namespace glg {
+	struct Vertex {
+		glm::vec3 position;
+		glm::vec3 normal;
+		glm::vec3 texCoords;
+	};
+
+	class Mesh {
+	public:
+		std::vector<Vertex>	vertices;
+		std::vector<unsigned int> indices;
+		std::vector<Texture2D> textures;
+
+		Mesh(std::vector<Vertex>& vertices, std::vector<unsigned int>& indices, std::vector<Texture2D>& textures) {
+			this->vertices = vertices;
+			this->indices = indices;
+			this->textures = textures;
+
+			setupMesh();
+		}
+
+		void draw(Shader& shader) {
+			
+		}
+
+	private:
+		unsigned int VAO, VBO, EBO;
+
+		void setupMesh() {
+			glGenVertexArrays(1, &VAO);
+			glGenBuffers(1, &VBO);
+			glGenBuffers(1, &EBO);
+
+			glBindVertexArray(VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+			
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+			// vertex positions
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+			// vertex texture coords
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoords));
+
+			glBindVertexArray(0);
+		}
+	};
+}
