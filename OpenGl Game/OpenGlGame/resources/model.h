fbd12fd26412333b5832484d63485fdf487330d7
BuildTools <unconfigured@null.spigotmc.org>
2021-08-29 10:32:53 -0400

    added loadModel method

--- OpenGl Game/OpenGlGame/resources/model.h
+++ OpenGl Game/OpenGlGame/resources/model.h
@@ -23,10 +23,35 @@ namespace glg {
 		std::vector<Mesh> meshes;
 		std::string directory;
 
-		void loadModel(const char* path);
+		void loadModel(std::string path) {
+			Assimp::Importer importer;
+			const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
-		void processNode(aiNode* node, const aiScene* scene);
+			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+				std::cout << "Could not load model " << importer.GetErrorString() << std::endl;
+				return;
+			}
+
+			directory = path.substr(0, path.find_last_of('/'));
+
+			processNode(scene->mRootNode, scene);
+		}
+
+		void processNode(aiNode* node, const aiScene* scene) {
+			// processes all the node's meshes
+			for (size_t i = 0; i < node->mNumMeshes; i++) {
+				aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+				meshes.push_back(processMesh(mesh, scene));
+			}
+
+			// processes all the nodes children
+			for (size_t i = 0; i < node->mNumChildren; i++) {
+				processNode(node->mChildren[i], scene);
+			}
+		}
+
 		Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+
 		std::vector<Texture2D> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName);
 	};
 }
