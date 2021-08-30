c14d98eaff389a107a24b184b4c87c5c2ef2d1c2
BuildTools <unconfigured@null.spigotmc.org>
2021-08-30 03:14:56 -0400

    added VisibleEntity class

--- OpenGl Game/OpenGlGame/entities/visible_entity.cpp
+++ OpenGl Game/OpenGlGame/entities/visible_entity.cpp
@@ -1,5 +1,24 @@
 #include "visible_entity.h"
 #include <vector>
 
+glg::VisibleEntity::VisibleEntity(glm::vec3 position, glm::quat rotation, Model model) : Entity(position, rotation), model(model)
+{
+}
 
+glg::VisibleEntity::VisibleEntity(glm::vec3 position, glm::vec3 rotation, Model model) : Entity(position, rotation), model(model)
+{
+}
 
+glm::mat4 glg::VisibleEntity::getModelMatrix()
+{
+	glm::mat4 modelMatrix = glm::mat4(1);
+	modelMatrix = glm::translate(modelMatrix, position);
+	modelMatrix = glm::toMat4(rotation) * modelMatrix;
+	return modelMatrix;
+}
+
+void glg::VisibleEntity::draw()
+{
+	getShader().setMat4("model", getModelMatrix());
+	model.draw(getShader());
+}
--- OpenGl Game/OpenGlGame/entities/visible_entity.h
+++ OpenGl Game/OpenGlGame/entities/visible_entity.h
@@ -1,15 +1,27 @@
 #pragma once
+#include "../resources/model.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 #include "entity.h"
 
 namespace glg {
 	class VisibleEntity : public Entity
 	{
 	public:
-		VisibleEntity(glm::vec3 position, glm::quat rotation); 
+		VisibleEntity(glm::vec3 position, glm::quat rotation, Model model);
 
+		VisibleEntity(glm::vec3 position, glm::vec3 rotation, Model model);
+
+		virtual glm::mat4 getModelMatrix();
+
+		virtual void draw();
 		
-
 	protected:
+		Model model;
+
+		virtual Shader& getShader() = 0;
+
 	};
 }
 
