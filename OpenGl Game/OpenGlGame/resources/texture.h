50c3a1d3b786f795294e308dfa0115c475f76c3f
BuildTools <unconfigured@null.spigotmc.org>
2021-08-30 03:18:11 -0400

    removed debug cout

--- OpenGl Game/OpenGlGame/resources/texture.h
+++ OpenGl Game/OpenGlGame/resources/texture.h
@@ -73,7 +73,6 @@ namespace glg {
 				}
 
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-				std::cout << glGetError() << texturePath << std::endl;
 				glGenerateMipmap(GL_TEXTURE_2D);
 			}
 			else {
