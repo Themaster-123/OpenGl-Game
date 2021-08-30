9d6d0c671332409a798b2ab8217dba8622f9e948
BuildTools <unconfigured@null.spigotmc.org>
2021-08-30 14:29:20 -0400

    added getModel method

--- OpenGl Game/OpenGlGame/entities/visible_entity.cpp
+++ OpenGl Game/OpenGlGame/entities/visible_entity.cpp
@@ -1,15 +1,15 @@
 #include "visible_entity.h"
 #include <vector>
 
-glg::VisibleEntity::VisibleEntity(glm::vec3 position, glm::quat rotation, Model model) : Entity(position, rotation), model(model)
+glg::VisibleEntity::VisibleEntity(glm::vec3 position, glm::quat rotation) : Entity(position, rotation)
 {
 }
 
-glg::VisibleEntity::VisibleEntity(glm::vec3 position, glm::vec3 rotation, Model model) : Entity(position, rotation), model(model)
+glg::VisibleEntity::VisibleEntity(glm::vec3 position, glm::vec3 rotation) : Entity(position, rotation)
 {
 }
 
-glm::mat4 glg::VisibleEntity::getModelMatrix()
+glm::mat4 glg::VisibleEntity::getModelMatrix() const
 {
 	glm::mat4 modelMatrix = glm::mat4(1);
 	modelMatrix = glm::translate(modelMatrix, position);
@@ -20,5 +20,11 @@ glm::mat4 glg::VisibleEntity::getModelMatrix()
 void glg::VisibleEntity::draw()
 {
 	getShader().setMat4("model", getModelMatrix());
-	model.draw(getShader());
+	getModel().draw(getShader());
+}
+
+void glg::VisibleEntity::update()
+{
+	draw();
+	std::cout << "eaa";
 }
--- OpenGl Game/OpenGlGame/entities/visible_entity.h
+++ OpenGl Game/OpenGlGame/entities/visible_entity.h
@@ -9,18 +9,21 @@ namespace glg {
 	class VisibleEntity : public Entity
 	{
 	public:
-		VisibleEntity(glm::vec3 position, glm::quat rotation, Model model);
+		VisibleEntity(glm::vec3 position, glm::quat rotation);
 
-		VisibleEntity(glm::vec3 position, glm::vec3 rotation, Model model);
+		VisibleEntity(glm::vec3 position, glm::vec3 rotation);
 
-		virtual glm::mat4 getModelMatrix();
+		virtual glm::mat4 getModelMatrix() const;
 
 		virtual void draw();
+
+		virtual void update();
 		
 	protected:
-		Model model;
 
 		virtual Shader& getShader() = 0;
+
+		virtual Model& getModel() = 0;
 
 	};
 }
