f212ea3f9b1a8d819adbfa25d982bbfff104f4fc
BuildTools <unconfigured@null.spigotmc.org>
2021-09-01 21:46:27 -0400

    added physics entity class

---
+++ OpenGl Game/OpenGlGame/entities/physics_entity.cpp
@@ -0,0 +1,67 @@
+#include "physics_entity.h"
+
+glg::PhysicsEntity::PhysicsEntity(glm::vec3 position, glm::quat rotation) : VisibleEntity(position, rotation)
+, prevTransform(getTransform())
+{
+}
+
+glg::PhysicsEntity::PhysicsEntity(glm::vec3 position, glm::vec3 rotation) : VisibleEntity(position, rotation)
+, prevTransform(getTransform())
+{
+}
+
+void glg::PhysicsEntity::setPosition(glm::vec3 position)
+{
+    VisibleEntity::setPosition(position);
+    collisionBody->setTransform(getTransform());
+}
+
+void glg::PhysicsEntity::setRotation(glm::quat rotation)
+{
+    VisibleEntity::setRotation(rotation);
+    collisionBody->setTransform(getTransform());
+}
+
+rp3d::Transform glg::PhysicsEntity::getTransform() const
+{
+    return rp3d::Transform(getPosition(), getRotation());
+}
+
+rp3d::Transform glg::PhysicsEntity::getInterpolatedTransform() const {
+    return rp3d::Transform::interpolateTransforms(prevTransform, getTransform(), std::min(factor, 1.0f));
+}
+
+glm::mat4 glg::PhysicsEntity::getModelMatrix() const
+{
+    glm::mat4 modelMatrix = glm::mat4(1);
+    rp3d::Transform transform = getInterpolatedTransform();
+    modelMatrix = glm::translate(modelMatrix, (glm::vec3) transform.getPosition());
+    modelMatrix = modelMatrix * glm::toMat4((glm::quat) transform.getOrientation());
+    return modelMatrix;
+}
+
+void glg::PhysicsEntity::update()
+{
+    VisibleEntity::update();
+    prevTransform = getTransform();
+}
+
+void glg::PhysicsEntity::physicsUpdate()
+{
+    updatePositionToBodyPosition();
+    updateRotationToBodyRotation();
+}
+
+void glg::PhysicsEntity::updatePositionToBodyPosition()
+{
+    rp3d::Transform collisionBodyTrans = collisionBody->getTransform();
+    setPosition(collisionBodyTrans.getPosition());
+}
+
+void glg::PhysicsEntity::updateRotationToBodyRotation()
+{
+    rp3d::Transform collisionBodyTrans = collisionBody->getTransform();
+    setRotation(collisionBodyTrans.getOrientation());
+}
+
+
---
+++ OpenGl Game/OpenGlGame/entities/physics_entity.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "visible_entity.h"
+#include <reactphysics3d/reactphysics3d.h>
+#include "../physics.h"
+
+namespace glg {
+	class PhysicsEntity : public VisibleEntity
+	{
+	public:
+		PhysicsEntity(glm::vec3 position, glm::quat rotation);
+
+		PhysicsEntity(glm::vec3 position, glm::vec3 rotation);
+
+		virtual void setPosition(glm::vec3 position);
+
+		virtual void setRotation(glm::quat rotation);
+
+		virtual rp3d::Transform getTransform() const;
+
+		virtual rp3d::Transform getInterpolatedTransform() const;
+
+		virtual glm::mat4 getModelMatrix() const;
+
+	protected:
+		rp3d::CollisionBody* collisionBody;
+		rp3d::Transform prevTransform;
+
+		virtual rp3d::CollisionBody* getCollisionBody() = 0;
+
+		virtual void update();
+
+		virtual void physicsUpdate();
+
+		virtual void updatePositionToBodyPosition();
+
+		virtual void updateRotationToBodyRotation();
+	};
+}
+
