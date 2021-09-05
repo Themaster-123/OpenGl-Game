93e8ca2a1378a21570d2497988a401b43eea072e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-05 18:29:15 -0400

    added two constructors to vertex struct

--- OpenGl Game/OpenGlGame/resources/mesh.h
+++ OpenGl Game/OpenGlGame/resources/mesh.h
@@ -11,6 +11,16 @@ namespace glg {
 		glm::vec3 position;
 		glm::vec3 normal;
 		glm::vec2 texCoords;
+
+		Vertex() {
+
+		}
+
+		Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texCoords) {
+			this->position = position;
+			this->normal = normal;
+			this->texCoords = texCoords;
+		}
 	};
 
 	class Mesh {
