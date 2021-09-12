47919edc860fc932314843a08f301a5052ad52cc
aidan gray <thepuppet260@gmail.com>
2021-09-12 16:14:48 -0400

    added lookable component

---
+++ OpenGl Game/OpenGlGame/components/lookable_component/lookable_component.cpp
@@ -0,0 +1,5 @@
+#include "lookable_component.h"
+
+glg::LookableComponent::LookableComponent(float pitch, float yaw) : pitch(pitch), yaw(yaw)
+{
+}
---
+++ OpenGl Game/OpenGlGame/components/lookable_component/lookable_component.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace glg {
+	struct LookableComponent
+	{
+		float pitch;
+		float yaw;
+
+		LookableComponent(float pitch = 0, float yaw = 0);
+
+	};
+}
+
