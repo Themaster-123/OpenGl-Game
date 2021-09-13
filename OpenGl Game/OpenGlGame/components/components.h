2491eede829fc999581b0b9b46249114b167c5f6
aidan gray <thepuppet260@gmail.com>
2021-09-13 17:04:48 -0400

    added light includes to components

--- OpenGl Game/OpenGlGame/components/components.h
+++ OpenGl Game/OpenGlGame/components/components.h
@@ -6,3 +6,8 @@
 #include "camera_component/camera_component.h"
 #include "player_component/player_component.h"
 #include "lookable_component/lookable_component.h"
+#include "light_components/light_component.h"
+#include "light_components/directional_light_component.h"
+#include "light_components/attenuation/attenuation_light_component.h"
+#include "light_components/attenuation/point_light_component.h"
+#include "light_components/attenuation/spot_light_component.h"
