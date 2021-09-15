1110f2228f4a355180a64643b8d37aa0e4cef69f
aidan gray <thepuppet260@gmail.com>
2021-09-15 01:24:43 -0400

    added some more chunks

--- OpenGl Game/OpenGlGame/world/world.cpp
+++ OpenGl Game/OpenGlGame/world/world.cpp
@@ -5,4 +5,7 @@ float glg::world::World::CHUNK_SIZE = 32.0f;
 glg::world::World::World()
 {
 	chunks.insert(std::pair<glm::ivec2, Chunk*>(glm::ivec2(0, 0), new Chunk(glm::ivec2(-1, -1)))); 
+	chunks.insert(std::pair<glm::ivec2, Chunk*>(glm::ivec2(0, 0), new Chunk(glm::ivec2(-1, -2))));
+	chunks.insert(std::pair<glm::ivec2, Chunk*>(glm::ivec2(0, 0), new Chunk(glm::ivec2(-2, -1))));
+	chunks.insert(std::pair<glm::ivec2, Chunk*>(glm::ivec2(0, 0), new Chunk(glm::ivec2(-2, -2))));
 }
