07700fc4a71878b0ce58b6b798ff7a2aba289a46
aidan gray <thepuppet260@gmail.com>
2021-09-16 03:11:19 -0400

    changed speed to 200

--- OpenGl Game/OpenGlGame/components/player_component/player_component.h
+++ OpenGl Game/OpenGlGame/components/player_component/player_component.h
@@ -5,7 +5,7 @@ namespace glg {
 		float sensitivity;
 		float speed = 10;
 
-		PlayerComponent(float sensitivity = .2f, float speed = 100);
+		PlayerComponent(float sensitivity = .2f, float speed = 200);
 
 		virtual PlayerComponent& operator=(const PlayerComponent& other);
 	};
