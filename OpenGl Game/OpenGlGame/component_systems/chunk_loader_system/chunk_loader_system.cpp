dc2a17637fa2dfdf1606ec9559a5a8e8cb698ad2
aidan gray <thepuppet260@gmail.com>
2021-09-16 14:23:26 -0400

    made it unload chunks

--- OpenGl Game/OpenGlGame/component_systems/chunk_loader_system/chunk_loader_system.cpp
+++ OpenGl Game/OpenGlGame/component_systems/chunk_loader_system/chunk_loader_system.cpp
@@ -27,8 +27,8 @@ void glg::ChunkLoaderSystem::update()
 			glm::ivec2 offsetPos(0, 0);
 			glm::ivec2 direction(0, -1);
 			glm::ivec2 loadPos;
-
-			int sizeSqrd = world::CHUNK_LOAD_SIZE * world::CHUNK_LOAD_SIZE;
+			std::cout << (world::CHUNK_LOAD_SIZE * 2) + 1;
+			int sizeSqrd = ((world::CHUNK_LOAD_SIZE * 2) + 1) * ((world::CHUNK_LOAD_SIZE * 2) + 1);
 			int moveAmount = 1;
 
 			for (int i = 0, increase = 0, amountMoved = 0; i < sizeSqrd; i++, amountMoved++) {
@@ -53,6 +53,21 @@ void glg::ChunkLoaderSystem::update()
 				}
 				
 				offsetPos += direction;
+			}
+
+			std::vector<glm::ivec2> chunksToDelete;
+
+			for (const auto& [pos, chunk] : scene::WORLD.chunks) {
+				glm::ivec2 localPos = chunkPos - pos;
+				int distance = std::max(abs(localPos.x), abs(localPos.y));
+
+				if (distance >= (world::CHUNK_LOAD_SIZE * 2) + 1) {
+					chunksToDelete.push_back(pos);
+				}
+			}
+
+			for (auto& pos : chunksToDelete) {
+				scene::WORLD.unloadChunk(pos);
 			}
 
 		}
